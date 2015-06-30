aef2d16762c6aeae87537b484c188f96ff18ac07
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-06-30 08:42:38 +0530

    added constructor for solver with argument errors

--- lib/collatz_conjecture/solver.rb
+++ lib/collatz_conjecture/solver.rb
@@ -1,3 +1,21 @@
 module CollatzConjecture
-  class Solver; end
+  class Solver
+    def initialize(initial, final)
+      unless initial >= 0 || final >= 0
+        raise ArgumentError.new(
+                  "initial (#{initial}) and final (#{final}) values" +
+                      " can't be negative")
+      end
+      unless initial >= 0
+        raise ArgumentError.new(
+                  "initial (#{initial}) value can't be negative")
+      end
+      unless final >= 0
+        raise ArgumentError.new(
+                  "final (#{final}) value can't be negative")
+      end
+      @initial = initial
+      @final   = final
+    end
+  end
 end
--- spec/collatz_conjecture/solver_spec.rb
+++ spec/collatz_conjecture/solver_spec.rb
@@ -1,5 +1,27 @@
 require_relative '../spec_helper'
 
 module CollatzConjecture
-  describe Solver do; end
+  describe Solver do
+    describe '#initialize' do
+      it 'should accept initial and final values as positive integers' do
+        expect(Solver.new(0, 2)).to_not be_nil
+      end
+
+      it 'should throw an ArgumentError if initial or final are negative' do
+        expect {
+          Solver.new(-2, 1)
+        }.to raise_error(ArgumentError,
+                         "initial (-2) value can't be negative")
+        expect {
+          Solver.new(2, -1)
+        }.to raise_error(ArgumentError,
+                         "final (-1) value can't be negative")
+        expect {
+          Solver.new(-2, -1)
+        }.to raise_error(
+                 ArgumentError,
+                 "initial (-2) and final (-1) values can't be negative")
+      end
+    end
+  end
 end
